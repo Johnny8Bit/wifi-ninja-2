eee0c4617c1e3db9b679bdaf40a51fc9566c32fb
Johnny8Bit <33725046+Johnny8Bit@users.noreply.github.com>
2023-06-16 23:02:28 +0200

    init

--- raspberry-pi/pi-scanner.py
+++ raspberry-pi/pi-scanner.py
@@ -3,7 +3,7 @@ import subprocess
 
 wlan_interface = "wlan0"
 
-scan_output = subprocess.call(["sudo", "iw", "dev", wlan_interface, "scan" ])
+scan_output = str(subprocess.call(["sudo", "iw", "dev", wlan_interface, "scan" ]))
 
 list = scan_output.split("BSS")
 print(list)
