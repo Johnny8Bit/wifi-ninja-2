f213ff6f0c6178151a3650fd6d545d00ea85774d
Johnny8Bit <33725046+Johnny8Bit@users.noreply.github.com>
2023-06-16 23:09:55 +0200

    init

--- raspberry-pi/pi-scanner.py
+++ raspberry-pi/pi-scanner.py
@@ -5,5 +5,5 @@ wlan_interface = "wlan0"
 
 scan_output = subprocess.run(["sudo", "iw", "dev", wlan_interface, "scan" ], capture_output=True)
 
-list = scan_output.stdout
+list = scan_output.stdout.decode('UTF-8')
 print(list)
