a4eaebbfcbda16ba9140415ae862dcef0a5c1f9a
Johnny8Bit <33725046+Johnny8Bit@users.noreply.github.com>
2023-06-17 18:24:27 +0200

    init

--- dashboard/ninja2-dashboard.py
+++ dashboard/ninja2-dashboard.py
@@ -1,23 +1,33 @@
-from flask import Flask, render_template, request
 import json
 
-web_monitor = Flask(__name__)
+from flask import Flask, render_template, request
 
-sensor_data = []
+DASHBOARD_API_KEY = "12345"
 
-@web_monitor.route('/PostMonitorData', methods = ['POST'])
+
+dashboard = Flask(__name__)
+
+sensor_data = {}
+
+
+@dashboard.route('/PostSensorData', methods = ['POST'])
 def data():
 
     global sensor_data
-    sensor_data = (json.loads(request.data))
-    return 'OK', 200
+    received_auth = request.headers.get("api_key")
+    if received_auth == DASHBOARD_API_KEY:
+        sensor_data.update(json.loads(request.data))
+        return 'OK', 200
+    else:
+        return 'Nope', 401
 
-@web_monitor.route('/', methods = ['GET'])
+
+@dashboard.route('/', methods = ['GET'])
 def index():
-    
-    return render_template('index.html', data_list = sensor_data)
+    print(sensor_data)
+    return render_template('index.html', data = sensor_data)
 
 
 if __name__ == '__main__':
 
-    web_monitor.run(host='0.0.0.0', port=80, debug=False)
+    dashboard.run(host='0.0.0.0', port=80, debug=False)
