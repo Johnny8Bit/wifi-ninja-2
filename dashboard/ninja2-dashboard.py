8ef40649ee74862430046ec6165226e567374280
Johnny8Bit <33725046+Johnny8Bit@users.noreply.github.com>
2023-06-20 18:01:48 +0200

    int

--- dashboard/ninja2-dashboard.py
+++ dashboard/ninja2-dashboard.py
@@ -11,6 +11,27 @@ dashboard = Flask(__name__)
 sensor_data = {}
 
 
+def remove_stale():
+
+    REMOVE_AFTER = 60 #seconds
+    stale_data = []
+
+    for key, value in sensor_data.items():
+        lastheard = []
+        date = value["sensor"]["sensor_lastheard"].split()[0].split("-")
+        time = value["sensor"]["sensor_lastheard"].split()[1].split(":")
+        for item in date + time:
+            if item.startswith("0"):
+                lastheard.append(int(item[1:]))
+            else:
+                lastheard.append(int(item))
+        if (datetime.now() - datetime(*lastheard)).seconds > REMOVE_AFTER:
+            stale_data.append(key)
+            
+    for sensor in stale_data:
+        del sensor_data[sensor]
+
+
 @dashboard.route('/PostSensorData', methods = ['POST'])
 def data():
 
@@ -29,6 +50,8 @@ def data():
 @dashboard.route('/', methods = ['GET'])
 def index():
 
+    remove_stale()
+        
     return render_template('index.html', data = sensor_data)
 
 
